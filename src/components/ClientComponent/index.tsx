4b7575821ced74f092d93d147b490db945c12fb4
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-08-10 18:32:24 +0900

    confirm that RSC works with Astro

---
+++ src/components/ClientComponent/index.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function ClientComponent() {
+  const [todos, setTodos] = useState<{ title: string }[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      const todos = (
+        await axios.get<{ title: string }[]>(
+          "https://jsonplaceholder.typicode.com/todos?userId=1"
+        )
+      ).data;
+      console.log({ todos });
+      setTodos(todos);
+    })();
+  });
+
+  console.log({ todos });
+
+  if (todos.length === 0) {
+    return <div>Loading todos...</div>;
+  }
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.title}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/ServerComponent/index.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export async function ServerComponent() {
+  const res = await axios.get<{ title: string }[]>(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  const todos = res.data;
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.title}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
