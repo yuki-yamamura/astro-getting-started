62a33638406e62c3acfaa77e2bc40ad928cbce11
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-08-11 11:39:49 +0900

    debug Cloudflare Pages

--- src/components/Name/index.tsx
+++ src/components/Name/index.tsx
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 
 export function Name() {
   useEffect(() => {
-    document.cookie = 'name=John';
+    document.cookie = 'name=Bob';
 
     return () => {
       document.cookie = '';
     };
   });
 
-  return <div>John</div>;
+  // return <div>John</div>;
+  return <div>this page is revised by Bob.</div>;
 }
